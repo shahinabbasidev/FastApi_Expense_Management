e205db4f6f9da289fdcbef1c84a9506de2cb1b34
shahinabbasidev <shahin.abbasi.dev@gmail.com>
2025-12-27 17:11:15 +0330

    change the schemas file validators

--- core/schemas.py
+++ core/schemas.py
@@ -4,23 +4,47 @@ import re
 
 class BasePersonSchema(BaseModel):
 
-    title : str = Field(...,description="Enter your expenses name")
+    first_name : str = Field(...,description="Enter your first name")
+    last_name : str = Field(...,description="Enter your last name")
+    age : int = Field(..., gt=0, description="Must be a positive number")
+    expense_name : str = Field(...,description="Enter your expenses name")
     mount : float = Field(..., gt=0, description="Must be a positive number")
 
-    @field_validator("title")
-    def validate_title(cls,value):
+    @field_validator("first_name")
+    def validate_first_name(cls,value):
         if len(value) >= 30:
+            raise ValueError("You must use less than 30 characters")
+        if not re.fullmatch(r"^[a-zA-Z]+$",value):
+            raise ValueError("Title can contain only letters, numbers, and underscore (_)")
+        return value
+
+    @field_serializer("first_name")
+    def serialize_name(self,value):
+        return value.title()
+    
+    @field_validator("last_name")
+    def validate_last_name(cls,value):
+        if len(value) >= 30:
+            raise ValueError("You must use less than 30 characters")
+        if not re.fullmatch(r"^[a-zA-Z]+$",value):
+            raise ValueError("Title can contain only letters, numbers, and underscore (_)")
+        return value
+            
+    @field_serializer("last_name")
+    def serialize_name(self,value):
+        return value.title()
+    
+    @field_validator("expense_name")
+    def validate_expense_name(cls,value):
+        if len(value) >= 40:
             raise ValueError("You must use less than 30 characters")
         if not re.fullmatch(r"^[a-zA-Z0-9_]+$",value):
             raise ValueError("Title can contain only letters, numbers, and underscore (_)")
         return value
-
-
-    @field_serializer("title")
+    
+    @field_serializer("expense_name")
     def serialize_name(self,value):
         return value.title()
-    
-
 
 class PersonCreateSchema(BasePersonSchema):
     pass
