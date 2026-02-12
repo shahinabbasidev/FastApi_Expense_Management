f0d331a3f3e0939801cab7622607d69cb39e0cf9
shahinabbasidev <shahin.abbasi.dev@gmail.com>
2026-02-12 20:38:26 +0330

    edited fr language file

--- compile_translations.py
+++ compile_translations.py
@@ -5,12 +5,18 @@ def generate_mo_from_po(po_path, mo_path):
     """Compile a .po file to .mo format using polib"""
     os.makedirs(os.path.dirname(mo_path), exist_ok=True)
     
-    # Load the .po file
-    po = polib.pofile(po_path)
-    
+    # Load the .po file explicitly using UTF-8 to avoid decoding issues
+    try:
+        po = polib.pofile(po_path, encoding='utf-8')
+    except TypeError:
+        # Older polib versions may not accept the encoding kwarg; read file text instead
+        with open(po_path, 'r', encoding='utf-8') as fh:
+            content = fh.read()
+        po = polib.pofile(content)
+
     # Save as .mo file
     po.save_as_mofile(mo_path)
-    print(f'✓ Compiled {mo_path}')
+    print(f'Compiled {mo_path}')
 
 # Compile the files
 base_path = 'core/locales'
