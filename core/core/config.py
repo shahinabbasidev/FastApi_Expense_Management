ef043c07413a7aaa6e765417a863668db1c58dc4
shahinabbasidev <shahin.abbasi.dev@gmail.com>
2026-02-27 12:52:44 +0330

    Update config.py

--- core/core/config.py
+++ core/core/config.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    SQLALCHEMY_DATABASE_URL: str
+    SQLALCHEMY_DATABASE_URL: str = "postgresql://user:pass@localhost/expenses"
     JWT_SECRET_KEY: str = "test"
-    REDIS_URL: str
+    REDIS_URL: str = "redis://redis:6379"
     # Sentry DSN / project key. Can be left empty for local development.
     SENTRY_DSN: str = "https://5052d08d8859c013ec9c3942d74b2158@sentry.hamravesh.com/9811"
     model_config = SettingsConfigDict(env_file=".env")
